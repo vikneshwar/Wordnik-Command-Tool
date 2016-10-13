5b9ccf54759da2cc0dee670f36651803defe2516
vikneshwar <lviknesh@gmail.com>
2016-10-14 02:54:19 +0530

    Added gaming mode

--- cli.js
+++ cli.js
@@ -4,24 +4,22 @@ var vorpal = require('vorpal')();
 var chalk = require('chalk');
 var figlet = require('figlet');
 var clear = require('clear');
-var cmdUtils = require('./cmdUtils.js');
+var cmdUtils = require('./lib/cmdUtils.js');
 var chalkTitle = chalk.bold.yellow.underline;
 var chalkValue = chalk.bold.green;
-
+var chalkRed = chalk.bold.red;
+var chalkWhite = chalk.white;
+var chalkGreen = chalk.green;
+var cliFormatter = require('./lib/cliFormatter');
+var utility = require('./lib/utility.js');
+// cliFormatter.log = vorpal.log;
 //command action for definition of a word
 vorpal
 	.command('def <word>','displays the definition of the word')
 	.action(function(args,cb){
 		cmdUtils.getDefintion(args.word)
 		.then(data => {
-			//this.log(data);
-			var info = JSON.parse(data);
-			info.forEach((item,index) => {
-				this.log(`${index+1}) ${chalkTitle(`Source`)}       : ${chalkValue(item.sourceDictionary)}`);
-				this.log(`${chalkTitle('Definition')}      : ${item.text}`);
-				this.log(`${chalkTitle('Parts of Speech')} : ${chalk.bold.magenta(item.partOfSpeech)}`);
-				this.log("\n");
-			});
+			cliFormatter.displayDefinition.call(this,JSON.parse(data));
 			cb();
 		})
 		.catch(err => {
@@ -35,10 +33,7 @@ vorpal
 	.action(function(args,cb){
 		cmdUtils.getSynonym(args.word)
 		.then(data => {
-			// this.log(data);
-			var info = JSON.parse(data);
-			var synonyms = info[0].words.join(", ");
-			this.log(`${chalkTitle('Synonym')} : ${synonyms}`);
+			cliFormatter.displaySynonym.call(this,JSON.parse(data));
 			cb();
 		})
 		.catch(err => {
@@ -52,13 +47,11 @@ vorpal
 	.action(function(args,cb){
 		cmdUtils.getAntonym(args.word)
 		.then(data => {
-			var info = JSON.parse(data);
-			var antonyms = info[0].words.join(", ");
-			this.log(`${chalkTitle('Antonyms')} : ${antonyms}`);
+			cliFormatter.displayAntonym.call(this,JSON.parse(data));
 			cb();
 		})
 		.catch(err => {
-			this.log(chalk.bold.red(err));
+			this.log(chalkRed(err));
 			cb();
 		});
 	});
@@ -69,13 +62,7 @@ vorpal
 	.action(function(args,cb){
 		cmdUtils.getExamples(args.word)
 		.then(data => {
-			// this.log(data);
-			var info = JSON.parse(data);
-			info.examples.forEach((item,index) => {
-				this.log(`${index+1}) ${chalkTitle(`Provider`)} : ${chalkValue(item.provider.name)}`);
-				this.log(`${chalkTitle(`Source`)}      : ${chalkValue(item.title)}`);
-				this.log(`${chalkTitle(`Example`)}     : ${item.text}`);
-			});
+			cliFormatter.displayExample.call(this,JSON.parse(data));
 			cb();
 		})
 		.catch(err => {
@@ -90,15 +77,58 @@ vorpal
 	.action(function(args,cb){
 		cmdUtils.getFullDict(args.word)
 		.then(data => {
-			var info = JSON.parse(data);
+			/*var defData = JSON.parse(data[0]);
+			var synData = JSON.parse(data[1]);
+			var antData = JSON.parse(data[2]);
+			var exaData = JSON.parse(data[3]);*/
+			this.log(chalkRed("Definition"));
+			cliFormatter.displayDefinition.call(this,JSON.parse(data[0]));
+			this.log("\n");
+			cliFormatter.displaySynonym.call(this,JSON.parse(data[1]));
+			this.log("\n");
+			cliFormatter.displayAntonym.call(this,JSON.parse(data[2]));
+			this.log("\n");
+			this.log(chalkRed("Example"));		
+			cliFormatter.displayExample.call(this,JSON.parse(data[3]));
 
+			cb();
+		})
+		.catch(err => {
+			this.log(err);
+			cb();
+		});
+	});
+
+
+vorpal
+	.command('dict today','displays definition, synonym, antonym, example for the word of the day')
+	.action(function(args,cb){
+		var word = "";
+		var info;
+		cmdUtils.getWordOfTheDay()
+		.then(data => {
+			info = JSON.parse(data);
+			word = info.word;
+			return Promise.all([cmdUtils.getSynonym(word),cmdUtils.getAntonym(word)]);
+		})
+		.then(data => {
+			/*var synData = JSON.parse(data[0]);
+			var antData = JSON.parse(data[1]);*/
+
+			this.log(chalkRed.bgWhite(`Word of the Day: ${chalkWhite.bgRed(`${word}`)}`));
+			this.log(chalkRed("Definition"));
+			cliFormatter.displayDefinition.call(this,info.definitions);
+			this.log(chalkRed("Synonym and Antonym"));
+			cliFormatter.displaySynonym.call(this,JSON.parse(data[0]));
+			cliFormatter.displayAntonym.call(this,JSON.parse(data[1]));
+			this.log(chalkRed("\nExample"));
+			cliFormatter.displayExample.call(this,info);
 			cb();
 		})
 		.catch(err => {
 			this.log(err);
 		});
 	});
-
 //command action to clear the console
 vorpal
 	.command('clear', 'clears the console')
@@ -117,3 +147,95 @@ vorpal
 			figlet.textSync('Wordnik')		
 		)
 	);
+var answerObj;
+var playing = false;
+vorpal
+	.mode('play','Enters into gaming mode')
+	.delimiter('$play$')
+	.init(function(args,cb){
+		this.log("Welcome to play mode !! Let's have some fun");
+		this.log(`${chalkRed.bgWhite('Here is how the game goes:')} 
+			1)The program will display definition, antonym or synonym and you need to guess the correct word
+			2)Synonym for the word is also accepted as right answer
+			3)If you provide a wrong word, 3 options are provided a)Try again b)hint c)quit
+			4)So, now start the game by typing start`);
+		cb();
+	})
+	.action(function(command,cb){
+		if(playing === true && command === "start") {
+			this.log(chalkRed(`Seems like you are already playing a game, to start a new one , quit the current game by typing ${chalkWhite.bgRed(`quit`)}`));
+			cb();
+		}else if(playing === true && command === "quit") {
+			this.log("You have quit the game");
+			playing = false;
+			cb();
+		}
+		else if(command === "start"){
+			playing = true;
+			cmdUtils.getRandomWord()
+			.then(response => {
+				// this.log(response);
+				
+				var data = {};
+				data.definition = JSON.parse(response[0]);
+				data.synonym = JSON.parse(response[1]);
+				data.antonym = JSON.parse(response[2]);
+				data.example = JSON.parse(response[3]); 
+				answerObj = data;
+
+				var displayData = utility.shuffleQuestion(data,answerObj);
+				this.log(`${chalkTitle(displayData.key)} : ${displayData.value}`);
+				cb();
+			}).catch(err => {
+				this.log(err);
+			});
+		}
+		else if(playing === true && command !=='start') {
+			// playing = true;
+			var valideList;
+			if(answerObj.synonym.length === 0)
+			 	valideList = [answerObj.definition[0].word];
+			 else
+			 	valideList = answerObj.synonym[0].words.concat([answerObj.definition[0].word]);
+
+			if(valideList.indexOf(command)!= -1){
+				this.log("Whoo!! Great , that was a right one");
+				playing = false;
+				cb();
+			} else {
+				this.log("That's wrong !!");
+
+				this.prompt([
+					{
+						type:'list',
+						name: 'options',
+						message: 'What do you want to do ?',
+						choices: ['Try again','Hint','Quit']		
+					}
+				]).then(answer => {
+					if(answer.options === "Try again"){
+						this.log("Great !! Seems like you don't give up");
+					} else if(answer.options === "Hint"){
+						this.log("Good move , here is a hint");
+						var hint = utility.getHint(answerObj);
+						this.log(`${chalkTitle(hint.key)} : ${hint.value}`);
+					} else {
+						this.log("Oops ! Sometimes you have to !");
+						this.log(`Word is: ${chalkRed.bgWhite(answerObj.definition[0].word)}`);
+						this.log(chalkRed("Definition"));
+						cliFormatter.displayDefinition.call(this,answerObj.definition);
+						this.log(chalkRed("Synonym and Antonym"));
+						cliFormatter.displaySynonym.call(this,answerObj.synonym);
+						cliFormatter.displayAntonym.call(this,answerObj.antonym);
+						playing = false;
+					}
+					cb();
+				});
+				
+			}
+		} else {
+			this.log(`Start a game by typing ${chalkRed.bgWhite('start')}`);
+			cb();
+		}
+	
+	});
